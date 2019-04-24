d4d99a2c47fc523eca443f50e00c2d9713bbc48d
Pasan Premaratne <pasan@premaratne.me>
2019-04-23 22:15:08 -0400

    S1V2

--- Arrays.playground/Contents.swift
+++ Arrays.playground/Contents.swift
@@ -2,4 +2,13 @@
 
 // Start with a set of empty square brackets - []
 // Add values between the brackets separating them using commas
+
 let daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+// Creating empty array literals
+// Explicitly specify type so compiler knows what data we will put in there
+// Both [String] and Array<String> can be used
+let tasks: [String] = []
+
+// Creates an array object
+let birthdayInviteList = Array<String>()
