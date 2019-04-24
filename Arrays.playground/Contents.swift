11b30300679f497db0b59d5484c9e53effd690ec
Pasan Premaratne <pasan@premaratne.me>
2019-04-23 22:03:54 -0400

    S1V1

--- Arrays.playground/Contents.swift
+++ Arrays.playground/Contents.swift
@@ -1,3 +1,5 @@
 // Arrays
 
-
+// Start with a set of empty square brackets - []
+// Add values between the brackets separating them using commas
+let daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
